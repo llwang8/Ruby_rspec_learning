1e613ceef80b192a9f4a48fdf9b645f2f0eb37f6
llwang8 <wangxbl56@gmail.com>
2016-01-24 18:52:23 -0500

    pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,5 +1,30 @@
 
-def translate()
-
-
+def translateword(word)
+	case word.downcase
+	when /^([aeiouy])/ #test for leading vowel
+		word.concat("ay")
+	when /^(qu+)(.*)/ #test for "qu"
+		$2 + $1 + "ay"
+	when /^([^aeiouy]+)(.*)/ #test for leading consonant
+		$2 + $1 + "ay"
+	when /^([^aeiouy]qu+)(.*)/ #test for leading consonant + "qu"  (not working?)
+		puts $2 + $1 + "ay"
+	else
+		word
+	end
 end
+
+def translate(phrase)
+	piglist = []
+	wordslist = phrase.split(" ")
+	wordslist.each do |w|
+		piglist << translateword(w)
+	end
+	puts "phrase: #{phrase}; \npig latin translation: #{piglist.inspect}"
+	piglatin = piglist.join(" ")
+end
+
+translate("apple banana cherry three school quiet square eat pie")
+
+#"apple".concat("ay") if /^([aeiouy])/.match("apple")
+# "square".replace ($2 + $1 + "ay")  if /^([^aeiouy]qu+)(.*)/.match("square")
