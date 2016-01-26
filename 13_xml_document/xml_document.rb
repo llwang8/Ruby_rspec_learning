b84626817a6e1e57b4ca13917eb456fdbd1e9a34
llwang8 <wangxbl56@gmail.com>
2016-01-25 23:21:19 -0500

    xml_document

---
+++ 13_xml_document/xml_document.rb
@@ -0,0 +1,44 @@
+class XmlDocument
+	def initialize(indent = false)
+		@indent = indent
+		@indent_level = 0
+		s = ""
+		s << "<xml/>"
+		s
+	end
+
+	def method_missing(method_name, *args, &block)
+		attributes = args[0] || {}
+		s = ""
+		s << (" " *@indent_level) if @indent
+		s << "<#{method_name}"
+		attributes.each_pair do |key, value|
+			s << " #{key}='#{value}'"
+		end
+		if block
+			s << ">"
+			s << "\n" if @indent
+			@indent_level += 1
+			s << yield
+			@indent_level -= 1
+			s << (" "*@indent_level) if @indent
+			s << "</#{method_name}>"
+			s << "\n" if indent
+		else
+			s << "</>"
+			s << "\n" if @indent
+		end
+		s
+	end
+
+	#def hello(*arg)
+	#	if arg.empty?
+	#		puts "<hello/>"
+	#	elsif arg.is_a?(Hash)
+	#		puts "<hello #{arg.keys}='#{arg[keys]}'>"
+	#	elsif arg.is_a?(block)
+	#		puts "<hello>yield</hello>"
+	#	end	
+	#end
+
+end
